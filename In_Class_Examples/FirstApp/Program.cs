ac9de1f153dbb3e05cf292a2dedd4c6c3c826599
Ackerman, Adam P <adamackrmn@gmail.com>
2023-01-26 10:58:58 -0600

    Completed an in class example

--- In_Class_Examples/FirstApp/Program.cs
+++ In_Class_Examples/FirstApp/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const string WELCOME_MESSAGE = "Welcome, to my application!";
+
+Console.WriteLine(WELCOME_MESSAGE);
+Console.WriteLine("What is your name? >>");
+string name;
+name = Console.ReadLine();
+
+//Console.WriteLine(name + ", what is your monthly income? >>");
+Console.WriteLine($"{name}, what is your monthly income? >>");
+string answer = Console.ReadLine();
+
+double monthlyIncome = Convert.ToDouble(answer);
+
+Console.WriteLine($"{name}, what is your monthly expenses? >>");
+answer = Console.ReadLine();
+
+double monthlyExpenses = Convert.ToDouble(answer);
+
+double total = monthlyIncome - monthlyExpenses;
+Console.WriteLine($"Monthly Income ({monthlyIncome.ToString("C")}) - Monthly Expenses ({monthlyExpenses.ToString("C")}) = {total.ToString("C3")}");
