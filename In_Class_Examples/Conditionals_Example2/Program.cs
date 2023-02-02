4f57b1c640dd01d49ac894a6568851e1d09e891a
Ackerman, Adam P <adam@ou.edu>
2023-02-02 10:49:59 -0600

    did example over fizzbuzz

---
+++ In_Class_Examples/Conditionals_Example2/Program.cs
@@ -0,0 +1,22 @@
+﻿int randomNumber = 0;
+
+Random r = new Random();
+randomNumber = r.Next(1, 100 + 1);
+
+randomNumber = 1;
+if (randomNumber % 3 == 0 && randomNumber % 5 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+}
+else if (randomNumber % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if (randomNumber % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else
+{
+    Console.WriteLine(randomNumber.ToString("N0"));
+}
