a316f9bea77c9823777d60e7fe79ab97a08c975e
Ackerman, Adam P <adam@ou.edu>
2023-02-07 11:34:58 -0600

    updated coin toss to be a loop

--- Participations/Conditional-CoinToss/Program.cs
+++ Participations/Conditional-CoinToss/Program.cs
@@ -1,35 +1,43 @@
 ﻿Random random = new Random();
-int randomNbr = random.Next(1, 3);
+bool shouldWeLetTheUserOutOfOurLoop = false;
 
-//Console.WriteLine(randomNbr);
-
-Console.WriteLine("Guess, Heads or Tails? <<");
-string usersResponse = Console.ReadLine();
-
-// Heads = 1
-// Tails = 2
-
-string coinTossAsWords;
-
-if (randomNbr == 1)
+do
 {
-    //Console.WriteLine("It was heads");
-    coinTossAsWords = "Heads";
-}
-else// if (randomNbr == 2)
-{
-    //Console.WriteLine("It was tails");
-    coinTossAsWords = "Tails";
-}
+    int randomNbr = random.Next(1, 3);
 
-Console.WriteLine("It was " + coinTossAsWords + ".");
+    Console.WriteLine(randomNbr);
+
+    Console.WriteLine("Guess, Heads or Tails? <<");
+    string usersResponse = Console.ReadLine();
+
+    // Heads = 1
+    // Tails = 2
+
+    string coinTossAsWords;
+
+    if (randomNbr == 1)
+    {
+        //Console.WriteLine("It was heads");
+        coinTossAsWords = "Heads";
+    }
+    else// if (randomNbr == 2)
+    {
+        //Console.WriteLine("It was tails");
+        coinTossAsWords = "Tails";
+    }
+
+    Console.WriteLine("It was " + coinTossAsWords + ".");
 
 
-if (coinTossAsWords == usersResponse)
-{
-    Console.WriteLine("Congratulations, you guessed correctly.");
-}
-else
-{
-    Console.WriteLine("Sorry, better luck next time.");
-}
+    if (coinTossAsWords == usersResponse)
+    {
+        Console.WriteLine("Congratulations, you guessed correctly.  You may now leave.");
+        shouldWeLetTheUserOutOfOurLoop = true;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, try again.");
+    }
+
+} while (shouldWeLetTheUserOutOfOurLoop == false);
+
