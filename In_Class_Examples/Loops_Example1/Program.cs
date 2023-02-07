59e42d146e506b281bc579b791331ad102950a7f
Ackerman, Adam P <adam@ou.edu>
2023-02-07 11:27:45 -0600

    did some in class examples

---
+++ In_Class_Examples/Loops_Example1/Program.cs
@@ -0,0 +1,28 @@
+﻿int countBy;
+Console.WriteLine("What number do you want to count by? >>");
+string answer = Console.ReadLine();
+//countBy = Convert.ToInt32(answer);
+bool isAnswerReallyANumber = int.TryParse(answer, out countBy);
+if (isAnswerReallyANumber == false)
+{
+    Console.WriteLine("Invalid input. Goodbye.");
+    Environment.Exit(1);
+    //return;
+}
+
+Console.WriteLine("What number do you want to count to? >>");
+int countTo = Convert.ToInt32(Console.ReadLine());
+
+int total = countBy;
+Console.WriteLine("While Loop");
+while (total <= countTo)
+{
+    Console.WriteLine(total.ToString("N"));
+    total = total + countBy;
+}
+
+Console.WriteLine("\nForLoop");
+for (int i = countBy; i <= countTo; i +=  countBy)
+{
+    Console.WriteLine(i.ToString("N0"));
+}
---
+++ In_Class_Examples/Loops_Example2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What number do you want to count down from? >>");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int total = 0;
+for (int i = value; i > -1; i--)
+{
+    Console.WriteLine(i.ToString("N0"));
+    total = total + i;
+}
+
+Console.WriteLine($"The total of all numbers i {total.ToString("N0")}");
