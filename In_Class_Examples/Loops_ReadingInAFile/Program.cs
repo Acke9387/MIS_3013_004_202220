fb4f8e29bea1a777623e618e421b0d262b9cc411
Ackerman, Adam P <adam@ou.edu>
2023-02-14 10:57:39 -0600

    did an in class example over reading in a file

---
+++ In_Class_Examples/Loops_ReadingInAFile/Program.cs
@@ -0,0 +1,25 @@
+﻿//string[] linesOfFile = File.ReadAllLines();
+
+//Gets us 1 string that is ALL of the contents of the file
+string contentsOfFile = File.ReadAllText("The stainless steel rat by Harry Harrison.txt");
+
+//Gets us a string for EACH line of the file --> MANY STRINGS
+string[] linesOfFile = File.ReadAllLines("The stainless steel rat by Harry Harrison.txt");
+
+//Console.WriteLine(linesOfFile[0]);
+//Console.WriteLine(linesOfFile[1]);
+//Console.WriteLine(linesOfFile[2]);
+
+for (int i = 0; i < linesOfFile.Length; i++)
+{
+    Console.WriteLine(linesOfFile[i]);
+    if (i % 5 == 0 && i != 0)
+    {
+        Console.ReadKey();
+    }
+}
+
+//Console.WriteLine(contentsOfFile);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.ReadKey();
