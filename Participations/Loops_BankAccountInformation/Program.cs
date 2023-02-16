f2a37ee8fd9e70ed49e5f9f937e544e6b9421044
Ackerman, Adam P <adam@ou.edu>
2023-02-16 11:08:53 -0600

    Completed the bank account information participation

---
+++ Participations/Loops_BankAccountInformation/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("How many bank accounts do you have? >>");
+string answer = Console.ReadLine();
+
+int numberOfAccounts;
+
+while (int.TryParse(answer, out numberOfAccounts) == false)
+{
+    Console.WriteLine("That value is invalid, please tell me a valid number of bank accounts >>");
+    answer = Console.ReadLine();
+}
+
+
+double total = 0;
+for (int i = 1; i <= numberOfAccounts; i++)
+{
+    Console.WriteLine($"What is your bank account balance for account #{i.ToString("N0")}");
+    double balance = Convert.ToDouble(Console.ReadLine());
+    total = total + balance;
+}
+
+
+Console.WriteLine($"Your total balance of all accounts is {total.ToString("C")}");
+
+double averageBalance = total / numberOfAccounts;
+
+if (averageBalance > 5000)
+{
+    Console.ForegroundColor =  ConsoleColor.Green; //(ConsoleColor)10
+}
+
+Console.WriteLine($"Your average balance of your accounts is {averageBalance.ToString("C")}");
+Console.ForegroundColor = ConsoleColor.White;
