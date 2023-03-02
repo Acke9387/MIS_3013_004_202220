1eade921e87ca69010cfbf122c28b7a33213d322
Ackerman, Adam P <adam@ou.edu>
2023-03-02 11:42:39 -0600

    Completed in class example over lists

---
+++ In_Class_Examples/Collections_Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> favoriteThings = new List<string>();
+
+
+do
+{
+
+    Console.WriteLine("Sir/Madaam, please tell me one of your favorite things >>");
+    string thing = Console.ReadLine();
+    favoriteThings.Add(thing);
+    //favoriteThings.Add(Console.ReadLine());
+    Console.WriteLine("Do you have another favorite thing to tell me about? yes or no >>");
+
+} while (Console.ReadLine().ToLower()[0] == 'y');
+
+Console.WriteLine("All your favorite things are:");
+
+
+foreach (var thing in favoriteThings)
+{
+    Console.WriteLine(thing);
+}
+
+Random rand = new Random();
+int randomThingIndex = rand.Next(0, favoriteThings.Count);
+
+Console.WriteLine($"A random thing from your list is {favoriteThings[randomThingIndex]}");
