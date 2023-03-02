89ede1911435a1a40437e0a67fe633474150c902
Ackerman, Adam P <adam@ou.edu>
2023-03-02 11:11:33 -0600

    completed string manipulation participation

---
+++ Participations/String_Manipulation/Program.cs
@@ -0,0 +1,27 @@
+﻿const string SENTENCE = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+Console.WriteLine(SENTENCE);
+
+Console.WriteLine("What word would you like to replace? >>");
+string searchWord = Console.ReadLine();
+
+Console.WriteLine($"What would you like to replace '{searchWord}' with? >>");
+string replaceWord = Console.ReadLine();
+
+bool sentenceContainsWord = SENTENCE.Contains(searchWord);
+if (sentenceContainsWord)
+{
+    string newSentence = SENTENCE.Replace(searchWord, replaceWord);
+    Console.WriteLine(newSentence);
+}
+else
+{
+    Console.WriteLine($"Sorry, I could not find your word '{searchWord}'.");
+
+    for (int i = searchWord.Length - 1; i >= 0; i = i -1)
+    {
+        Console.Write(searchWord[i]);
+    }
+}
+
+
