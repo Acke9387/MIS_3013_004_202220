4574f3beef4f80f9e620108e06340129f76f773f
Ackerman, Adam P <adam@ou.edu>
2023-03-07 10:52:06 -0600

    did an in class example over dictionaries

---
+++ In_Class_Examples/Collections_Dictionaries/Program.cs
@@ -0,0 +1,41 @@
+﻿Dictionary<string, double> students = new Dictionary<string, double>();
+
+do
+{
+    Console.WriteLine("What is the student's ID? >>");
+    string id = Console.ReadLine();
+
+    Console.WriteLine($"What is {id}'s GPA? >>");
+    double gpa = Convert.ToDouble(Console.ReadLine());
+
+    if (students.ContainsKey(id) == false)
+    {
+        students.Add(id, gpa);
+
+    }
+    else
+    {
+        Console.WriteLine($"The student with {id} already exists.  We will now update the {id} with the value {gpa}");
+
+        students[id] = gpa;
+    }
+
+    Console.WriteLine("Do you have another student to add? yes or no >>");
+} while (Console.ReadLine().ToLower() == "yes");
+
+Console.WriteLine("Which student do you want to see the GPA of? >>");
+foreach (var id in students.Keys)
+{
+    Console.WriteLine(id);
+}
+
+string answer = Console.ReadLine();
+
+if (students.ContainsKey(answer) == true)
+{
+    Console.WriteLine($"{answer}'s GPA is {students[answer].ToString("n2")}"); 
+}
+else
+{
+    Console.WriteLine("Sorry that student does not exist.");
+}
