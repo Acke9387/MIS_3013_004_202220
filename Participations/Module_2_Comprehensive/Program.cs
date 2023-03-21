910b0873616924ef9b1231409551912d694a0274
Ackerman, Adam P <adam@ou.edu>
2023-03-21 11:30:42 -0500

    completed partcipation

---
+++ Participations/Module_2_Comprehensive/Program.cs
@@ -0,0 +1,35 @@
+﻿List<string> states = new List<string>();
+
+do
+{
+    Console.WriteLine("Name a state where you have lived >>");
+    states.Add(Console.ReadLine());
+
+    Console.WriteLine("Do you have another state to add? yes or no <<");
+
+} while (Console.ReadLine().ToLower() == "yes");
+
+
+int max = 0;
+
+foreach (string state in states)
+{
+    if (state.Length > max)
+    {
+        max = state.Length;
+    }
+}
+
+foreach (string state in states)
+{
+
+    if (state.Length == max)
+    {
+        Console.WriteLine($"{state.ToUpper()} - {state[0]}{state[1]}");
+
+        //OR
+        Console.WriteLine($"{state.ToUpper()} - {state.Substring(0,2)}");
+
+    }
+
+}
