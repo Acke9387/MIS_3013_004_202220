64db1aff6c97d7d4f4f1b4e42286179a6b6065b8
Ackerman, Adam P <adam@ou.edu>
2023-04-18 10:45:48 -0500

    did toy participation

---
+++ Participations/Classes_Toy/Program.cs
@@ -0,0 +1,25 @@
+﻿using Classes_Toy;
+
+List<Toy> toybox = new List<Toy>();
+
+Toy toy = new Toy();
+toy.Manufacturer = "Hasbro";
+toy.Name = "Buzz";
+toy.Price = 50.25;
+toy.SetNotes("Disney");
+
+//Console.WriteLine(toy);
+
+toybox.Add(toy);
+
+Toy toy2 = new Toy("Lots of fun!");
+toy2.Manufacturer = "Hasbro";
+toy2.Name = "Slinky Dog";
+toy2.Price = 25.99;
+
+toybox.Add(toy2);
+
+foreach (Toy t in toybox)
+{
+    Console.WriteLine(t);
+}
---
+++ Participations/Classes_Toy/Toy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Notes;
+
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
+        }
+
+        public Toy(string note) : this()
+        {
+            //Manufacturer = string.Empty;
+            //Name = string.Empty;
+            //Price = 0;
+            //Notes = string.Empty;
+            Notes = note;
+        }
+
+        public string GetAisle()
+        {
+            string aisleLetter = Manufacturer.ToUpper()[0].ToString();
+            Random r = new Random();
+
+            string aisle = aisleLetter + r.Next(1, 25);
+
+            return aisle;
+        }
+
+        public string GetNotes()
+        {
+            return Notes;
+        }
+
+        public void SetNotes(string note)
+        {
+            Notes = note;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} made by {Manufacturer} is {Price.ToString("C")} and can be found on aisle {GetAisle()}. \tNotes:{Notes}";
+        }
+
+    }
+}
